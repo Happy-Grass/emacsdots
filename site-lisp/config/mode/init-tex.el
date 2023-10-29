a26321c2da2dcf274ffe16c25b2f47fda707b0d0
Happy-Grass <3564494961@qq.com>
2023-10-29 15:59:07 +0800

    Add auctex

--- site-lisp/config/mode/init-tex.el
+++ site-lisp/config/mode/init-tex.el
@@ -1,1 +1,15 @@
+(require 'tex)
+(load "auctex.el" nil t t)
+(setq Tex-auto-save t)
+(setq Tex-parse-self t)
+(setq-default Tex-master nil)
+
+(add-hook 'Latex-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+
+(setq TeX-PDF-mode t)
+(setq Tex-source-correlate-mode t)
+(setq Tex-source-correlate-method 'synctex)
+(setq Tex-view-program-selection '((output-pdf "PDF Tools")))
+(setq Tex-view-program-list '(("PDF Tools" Tex-pdf-tools-sync-view)))
 (provide 'init-tex)
