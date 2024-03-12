f0be5806650af00258301a8d6dbcc3436125b022
xfwahss <xfwahss@qq.com>
2024-03-12 17:45:07 +0800

    Edit modeline config

--- site-lisp/config/ui/init-modeline.el
+++ site-lisp/config/ui/init-modeline.el
@@ -1,7 +1,9 @@
 (require 'awesome-tray)
-(awesome-tray-mode 1)
+(setq awesome-tray-activate-modules '("location" "pdf-view-page" "belong" "file-path" "mode-name" "last-command" "battery" "date"))
 (setq awesome-tray-date-format "%m-%d %-H:%-M")
 (setq awesome-tray-info-padding-right 1) ;; Avoid warpping
+(setq awesome-tray-git-show-status t)
+(awesome-tray-mode 1)
 (setq awesome-tray-mode-line-active-color "SkyBule3")
 ;(setq awesome-tray-position "right")
 (provide 'init-modeline)
