d7aa01f94c6bb9362db4f75a04fc0e108ca3796d
xfwahss <xfwahss@qq.com>
2024-03-18 16:45:25 +0800

    Update config for ebib

--- site-lisp/config/mode/init-ebib.el
+++ site-lisp/config/mode/init-ebib.el
@@ -6,14 +6,24 @@
 (setq ebib-default-directory "F:/BaiduSyncdisk/Notes/ebib")
 (setq ebib-bib-search-dirs "F:/BaiduSyncdisk/Notes/ebib/bib")
 (setq ebib-notes-directory "F:/BaiduSyncdisk/Notes/roam/article")
+(setq ebib-file-search-dirs '("F:/BaiduSyncdisk/Notes/ebib/pdf"))
 (setq ebib-extra-fields
       '((biblatex "crossref" "xdata" "annotation" "abstract" "keywords" "file" "timestamp")
 	(BibTeX "crossref" "annote" "abstract" "keywords" "file" "if" "timestamp" "url" "doi" "issn")))
 (setq ebib-index-columns
-      '(("Author/Editor" 10 t)
+      '( ("Entry Key" 40 t) 
+	("Author/Editor" 10 t)
 	("Year" 6 t)
 	("if" 4 t)
 	("Title" 50 t)
 	))
 (setq ebib-preload-bib-files '("F:/BaiduSyncdisk/Notes/ebib/bib/default.bib"))
+(setq ebib-citation-commands
+      (quote ((org-mode (("cite" "\\cite{%K}"))))))
+(setq ebib-citation-insert-multiple t)
+(setq ebib-file-associations nil) ;;不设置外部程序,默认用pdf-tools打开
+;; 配置auto-generate-key
+(setq bibtex-autokey-expand-strings t)
+(setq bibtex-autokey-year-length 4)
+(setq bibtex-autokey-year-title-separator "_")
 (provide 'init-ebib)
