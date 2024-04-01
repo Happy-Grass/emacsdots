6df1f99c01d53c8da0fb70a47feec7ea749d1750
xfwahss <xfwahss@qq.com>
2024-04-01 18:13:08 +0800

    Update init-env

--- site-lisp/config/core/init-env.el
+++ site-lisp/config/core/init-env.el
@@ -4,7 +4,10 @@
 (setenv "PATH" (concat "C:/texlive/2023/bin/windows;" (getenv "PATH")))
 (setenv "PATH" (concat "C:/Program Files/IrfanView;" (getenv "PATH")))
 (setenv "PATH" (concat "D:/GreenSoftware/Scoop/apps/ImageMagick/current;" (getenv "PATH")))
-(setenv "PATH" (concat "D:/GreenSoftware/Scoop/apps/flameshot/current\bin;" (getenv "PATH")))
+(setenv "PATH" (concat "D:/GreenSoftware/Scoop/apps/flameshot/current/bin;" (getenv "PATH")))
+(setenv "PATH" (concat "C:/Windows/System32/OpenSSH;" (getenv "PATH")))
+(setenv "PATH" (concat (expand-file-name "~") "/.emacs.d/site-lisp/win_software/plink;" (getenv "PATH")))
+(setenv "PATH" (concat (expand-file-name "~") "/.emacs.d/site-lisp/win_software/base64;" (getenv "PATH")))
 (setenv "GIT_INSTALL_ROOT" "D:/GreenSoftware/Scoop/apps/git/current")
 
 (setq exec-path (split-string (getenv "PATH") ";"))
