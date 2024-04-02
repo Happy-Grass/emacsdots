fae7ab5f89b5f5198dde956f8a22abf9aca421e6
xfwahss <xfwahss@qq.com>
2024-04-02 18:43:38 +0800

    Update for eaf

--- site-lisp/config/extra/init-eaf.el
+++ site-lisp/config/extra/init-eaf.el
@@ -1,4 +1,10 @@
 (setq eaf-python-command "~/.emacs.d/site-lisp/extensions/emacs-python/python.exe")
+(setq holo-layer-python-command "~/.emacs.d/site-lisp/extensions/emacs-python/python.exe")
+(require 'holo-layer)
+(setq holo-layer-enable-indent-rainbow t
+      holo-layer-hide-mode-line t)
+(holo-layer-enable)
 (require 'eaf);; Need wmctrl
 (require 'eaf-browser)
 (provide 'init-eaf)
+
