c0ec8ef438cc903ef5148d3ea46c4bf086c149dc
xfwahss <xfwahss@qq.com>
2024-04-03 20:00:06 +0800

    update

--- site-lisp/config/extra/init-eaf.el
+++ site-lisp/config/extra/init-eaf.el
@@ -1,10 +1,16 @@
+(require 'init-python-path)
 (setq eaf-python-command "~/.emacs.d/site-lisp/extensions/emacs-python/python.exe")
+
+(require 'eaf);; Need wmctrl
+(require 'eaf-browser)
+
+
+
 (setq holo-layer-python-command "~/.emacs.d/site-lisp/extensions/emacs-python/python.exe")
 (require 'holo-layer)
 (setq holo-layer-enable-indent-rainbow t
       holo-layer-hide-mode-line t)
 (holo-layer-enable)
-(require 'eaf);; Need wmctrl
-(require 'eaf-browser)
 (provide 'init-eaf)
+
 
