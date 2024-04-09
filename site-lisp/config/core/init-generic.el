0ad2ed6bf4608e2ddb5b0d7181b57136ad5ca9f9
Happy-Grass <3564494961@qq.com>
2024-04-09 22:30:33 +0800

    update coding system

--- site-lisp/config/core/init-generic.el
+++ site-lisp/config/core/init-generic.el
@@ -49,6 +49,13 @@
 (set-default-coding-systems 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(modify-coding-system-alist 'process "*" 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
+(setq-default pathname-coding-system 'utf-8)
+(set-file-name-coding-system 'utf-8)
 ;; 平滑地进行半屏滚动，避免滚动后recenter操作
 (setq scroll-step 1
       scroll-conservatively 10000)
