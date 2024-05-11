37e3b590165a55b2d43b8722b1c091baad20490c
xfwahss <xfwahss@qq.com>
2024-05-11 13:42:36 +0800

    update init-generic

--- site-lisp/config/core/init-generic.el
+++ site-lisp/config/core/init-generic.el
@@ -26,14 +26,12 @@
 (setq split-width-threshold 30)
 (setq inhibit-compacting-font-caches t) ;使用字体缓存，避免卡顿
 (setq confirm-kill-processes nil)       ;退出自动杀掉进程
-(setq async-bytecomp-allowed-packages nil) ;避免magit报错
 (setq word-wrap-by-category t)             ;按照中文折行
 (setq make-backup-files nil)               ;不要烦人的备份文件
 
 (setq completion-auto-select nil)       ;避免默认自动选择
 
 (setq ad-redefinition-action 'accept)   ;不要烦人的 redefine warning
-(setq frame-resize-pixelwise t) ;设置缩放的模式,避免Mac平台最大化窗口以后右边和下边有空隙
 (setq frame-title-format
       '(buffer-file-name (:eval (abbreviate-file-name buffer-file-name))
          (dired-directory dired-directory "%b"))) ;; 设置title-bar显示文件的全路径
@@ -41,24 +39,18 @@
 ;; 设置默认编码为utf－8
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
-(set-clipboard-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(modify-coding-system-alist 'process "*" 'utf-8)
-(setq default-process-coding-system '(utf-8 . utf-8))
-(set-default 'process-coding-system-alist
-             '(("[cC][mM][dD][pP][rR][oO][xX][yY]" utf-8 . gbk-dos))) ;; 路径中文编码问题
-(setq-default pathname-coding-system 'utf-8)
-(set-file-name-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(if (eq system-type 'windows-nt)
+	(progn
+	  (set-selection-coding-system 'utf-16le-dos))
+  (set-selection-coding-system 'utf-8))
 ;; 平滑地进行半屏滚动，避免滚动后recenter操作
 (setq scroll-step 1
       scroll-conservatively 10000)
 
 
 (global-hl-line-mode)  ;高亮当前行
-;(menu-bar-bottom-window-divider) ;分离线，下面的
 ;; 配置fringe-mode
 (show-paren-mode 0)
 (global-eldoc-mode -1)
